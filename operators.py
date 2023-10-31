afb4c435eabf9526d9253a510155f600ad57e85f
Rabaya Rabu <70410637+rabayarabu@users.noreply.github.com>
2023-10-31 12:56:15 +0600

    Add variable declaration rules

---
+++ operators.py
@@ -0,0 +1,21 @@
+# variable, operator, constant, function
+# python variable declaration rules
+# 1. must start with lowercase or underscore _
+# 2. must start with letters, numbers and underscores _
+# 3. case sensitive
+
+x = 25 # assignment statement
+y = 5 # assignment statement
+
+sum = x + y # assignment with expression
+comparison = x - y # assignment with expression
+result = x * y # assignment with expression
+divided = int(x / y) # assignment with expression
+mod = x % y # assignment with expression
+
+print(sum) # print statement - addition
+print(comparison) # print statement - subtraction
+print(result) # print statement - multiplication
+print(divided) # print statement - division
+print(mod) # print statement - remainder
+print(2 ** 2) # print statement - power
