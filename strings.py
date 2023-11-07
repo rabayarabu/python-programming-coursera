7c348779997d86a678b952a0f0465c62ab8a38ec
Rabaya Rabu <70410637+rabayarabu@users.noreply.github.com>
2023-11-07 17:46:52 +0600

    Learned python stings

---
+++ strings.py
@@ -0,0 +1,14 @@
+# name = "Rabu"
+# index = 0
+# while index < len(name):
+#     letter = name[index]
+#     print(index, letter)
+#     index = index + 1 
+# using definite for loop is much more elegant
+# iteration varriables are taken care of by for loop
+
+name = "Rabu"
+for letter in name:
+    print(letter)
+
+# coursera / python data structure/ start with strings / chapter 6
