e27984ce91ee21cdb6ff2551a63017350863e74e
Rabaya Rabu <70410637+rabayarabu@users.noreply.github.com>
2023-11-10 23:35:52 +0600

    Add documentation to find-string.py

--- find-string.py
+++ find-string.py
@@ -3,7 +3,11 @@
 # extracted value to a floating point number and print it out.
 
 text = "X-DSPAM-Confidence:    0.8475"
+# find the starting of float number
 start_pos = text.find(':')
+# remove the whitespace before the float number
 number_str = text[start_pos+1:].strip()
+# convert string into float number
 float_number = float(number_str)
+# print the float number that found
 print(float_number)
