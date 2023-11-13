abb24965bb5f764f9af540ae0da6f6a473e7bf06
Rabaya Rabu <70410637+rabayarabu@users.noreply.github.com>
2023-11-13 12:36:20 +0600

    Process sample file

---
+++ process-sample.py
@@ -0,0 +1,13 @@
+# Prompting the user for a file name
+file_name = input("Enter the file name: ")
+try:
+    # Opening the file
+    with open(file_name, 'r') as file:
+        # Reading the contents and printing in upper case after removing trailing whitespaces
+        for line in file:
+            print(line.rstrip().upper())
+
+except FileNotFoundError:
+    print(f"File '{file_name}' not found. Please enter a valid file name.")
+except Exception as e:
+    print(f"An error occurred: {e}")
