61567bfbdd6a299c49c840a5aa7afa7dc5bc974e
Rabaya Rabu <70410637+rabayarabu@users.noreply.github.com>
2023-11-14 19:20:33 +0600

    Manipulate lists & strings

--- manipulating-lists.py
+++ manipulating-lists.py
@@ -9,8 +9,21 @@ print(sum(nums))
 print(sum(nums)/len(nums))
 
 greet = "Hello Everyone!"
-split_gtreet = greet.split()
+split_gtreet = greet.split() # split() breaks string into parts and produces list of strings
 for w in split_gtreet:
-    print(w)
+    print(w)  # we can access a particular word or loop through all the words
 print(split_gtreet)
 print(len(split_gtreet))
+
+line = 'A lot of                spaces'
+etc = line.split()
+print(etc)
+
+line = 'first;second;third'
+things = line.split()
+print(things)
+thing = line.split(';')
+print(thing)
+
+# When you do not specify delimiter, multiple spaces are treated as one delimiter.
+# You can specify what delimiter character to use in the spliting().
