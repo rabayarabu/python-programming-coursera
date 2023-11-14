6aa318b0e00edb338e9b32a6eaec73c9a57b4f35
Rabaya Rabu <70410637+rabayarabu@users.noreply.github.com>
2023-11-14 18:23:29 +0600

    Used built-in functions & lists

---
+++ manipulating-lists.py
@@ -0,0 +1,9 @@
+# Built-in functions and lists
+# There are number of functions in python that takes lists as parameters
+
+nums = [1, 2, 3, 4, 5, 6]
+print(len(nums))
+print(max(nums))
+print(min(nums))
+print(sum(nums))
+print(sum(nums)/len(nums))
