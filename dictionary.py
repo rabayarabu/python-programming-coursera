29c09b251a77d18b62fe30a9bf4546bb6a74d150
Rabaya Rabu <70410637+rabayarabu@users.noreply.github.com>
2023-12-17 23:50:55 +0600

    Delete a list of keys from a dictionary

---
+++ dictionary.py
@@ -0,0 +1,14 @@
+# Delete a list of keys from a dictionary
+
+sample_dict = {
+    "name": "Kelly",
+    "age": 25,
+    "salary": 8000,
+    "city": "New york"
+}
+# Keys to remove
+keys = ["name", "salary"]
+
+for k in keys:
+    sample_dict.pop(k)
+print(sample_dict)
